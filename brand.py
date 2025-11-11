fd0c43ad3cdf1eadd203f8e5ff24bafde45d74e1
aleafs <zhangxc83@gmail.com>
2025-11-11 23:37:08 +0800

    开发更新

--- brand.py
+++ brand.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
 
 import os
+import yaml
 
 class Config:
-    title: str
-    vendor: str
-    prefix: str
-    logo: str
+    title = 'Bluepipe'
+    prefix = 'bluepipe'
+    vendor = '杭州萃蓝网络科技有限公司'
+    logo = None
 
     def __init__(self, path: str):
-        self.title='Bluepipe'
-        self.vendor='杭州萃蓝网络科技有限公司'
-        self.prefix='bluepipe'
-        self.logo=''
+        with open(os.path.join(path, 'config.yaml'), 'r', encoding='utf-8') as f:
+            meta = yaml.safe_load(f.read())
+            print(meta)
 
         if os.path.isdir(path):
             for name in os.listdir(path):
                 print(name)
+                if name.startswith('logo.'):
+                    self.logo = os.path.join(path, name)
 
 if __name__ == '__main__':
     config = Config('/product')
